cbf6a410150ffa7b6dfb95d6231e436f4472aaba
Arthur Ragimov <arthur.ragimov@gmail.com>
2020-04-11 12:13:31 +0300

    Updated users migration fields

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,6 +17,11 @@ class CreateUsersTable extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
+            $table->string('phone')->nullable();
+            $table->string('firstname');
+            $table->string('lastname');
+            $table->string('patronymic');
+            $table->string('role');
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
