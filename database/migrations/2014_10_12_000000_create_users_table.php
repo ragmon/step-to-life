f64eb50c970a934069ac8465ed3e925345bd4b09
Arthur Ragimov <arthur.ragimov@gmail.com>
2020-04-11 12:26:28 +0300

    Released user:create command Updated User model fillable array Removed "name" from user migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,7 +15,6 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
             $table->string('email')->unique();
             $table->string('phone')->nullable();
             $table->string('firstname');
