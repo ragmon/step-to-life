ddc6ba3b37137d9cb4d4337c4f17570c1c972a44
Arthur Ragimov <arthur.ragimov@gmail.com>
2020-04-24 20:08:11 +0300

    added validation to the users.index create modal

--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -32,7 +32,7 @@
     </div>
 
     <!-- User Create modal -->
-    <div class="modal fade" id="modal-user-create">
+    <form class="modal fade" id="modal-user-create">
         <div class="modal-dialog">
             <div class="modal-content">
                 <div class="modal-header">
@@ -42,45 +42,43 @@
                     </button>
                 </div>
                 <div class="modal-body">
-                    <form role="form">
-                        <div class="card-body">
-                            <div class="form-group">
-                                <label for="user-create-email">E-mail</label>
-                                <input name="email" type="email" class="form-control" id="user-create-email" placeholder="example@example.com">
-                            </div>
-                            <div class="form-group">
-                                <label for="user-create-firstname">Имя</label>
-                                <input name="firstname" type="text" class="form-control" id="user-create-firstname" placeholder="Иван">
-                            </div>
-                            <div class="form-group">
-                                <label for="user-create-lastname">Фамилия</label>
-                                <input name="lastname" type="text" class="form-control" id="user-create-lastname" placeholder="Иванов">
-                            </div>
-                            <div class="form-group">
-                                <label for="user-create-patronymic">Отчество</label>
-                                <input name="patronymic" type="text" class="form-control" id="user-create-patronymic" placeholder="Иванович">
-                            </div>
-                            <div class="form-group">
-                                <label for="user-create-role">Должность</label>
-                                <input name="role" type="text" class="form-control" id="user-create-role" placeholder="консультант">
-                            </div>
-                            <div class="form-group">
-                                <label for="user-create-phone">Телефон</label>
-                                <input name="phone" type="text" class="form-control" id="user-create-phone" placeholder="+380123456789">
-                            </div>
+                    <div class="card-body">
+                        <div class="form-group">
+                            <label for="user-create-email">E-mail</label>
+                            <input name="email" type="email" class="form-control" id="user-create-email" placeholder="example@example.com">
                         </div>
-                        <!-- /.card-body -->
-                    </form>
+                        <div class="form-group">
+                            <label for="user-create-firstname">Имя</label>
+                            <input name="firstname" type="text" class="form-control" id="user-create-firstname" placeholder="Иван">
+                        </div>
+                        <div class="form-group">
+                            <label for="user-create-lastname">Фамилия</label>
+                            <input name="lastname" type="text" class="form-control" id="user-create-lastname" placeholder="Иванов">
+                        </div>
+                        <div class="form-group">
+                            <label for="user-create-patronymic">Отчество</label>
+                            <input name="patronymic" type="text" class="form-control" id="user-create-patronymic" placeholder="Иванович">
+                        </div>
+                        <div class="form-group">
+                            <label for="user-create-role">Должность</label>
+                            <input name="role" type="text" class="form-control" id="user-create-role" placeholder="консультант">
+                        </div>
+                        <div class="form-group">
+                            <label for="user-create-phone">Телефон</label>
+                            <input name="phone" type="text" class="form-control" id="user-create-phone" placeholder="+380123456789">
+                        </div>
+                    </div>
+                    <!-- /.card-body -->
                 </div>
                 <div class="modal-footer justify-content-between">
                     <button type="button" class="btn btn-default" data-dismiss="modal">Закрыть</button>
-                    <button type="button" class="btn btn-success btn-create">Создать</button>
+                    <button type="submit" class="btn btn-success btn-create">Создать</button>
                 </div>
             </div>
             <!-- /.modal-content -->
         </div>
         <!-- /.modal-dialog -->
-    </div>
+    </form>
     <!-- /.modal -->
 @stop
 
@@ -89,15 +87,57 @@
         $(function () {
             let $modalUserCreate = $('#modal-user-create');
 
-            $modalUserCreate.find('.btn-create').click(function () {
-                $.ajax({
-                    url : '{{ route('users.store') }}',
-                    method : 'POST',
-                    data : $modalUserCreate.find('form').serialize(),
-                    success : function (data) {
-                        location.reload();
-                    }
-                })
+            $modalUserCreate.validate({
+                submitHandler: function () {
+                    $.ajax({
+                        url : '{{ route('users.store') }}',
+                        method : 'POST',
+                        data : $modalUserCreate.serialize(),
+                        success : function (data) {
+                            location.reload();
+                        }
+                    })
+                },
+                rules: {
+                    email: {
+                        required: true,
+                    },
+                    firstname: {
+                        required: true,
+                    },
+                    lastname: {
+                        required: true,
+                    },
+                    patronymic: {
+                        required: true,
+                    },
+                    role: {
+                        required: true,
+                    },
+                    phone: {
+                        required: true,
+                    },
+                },
+                messages: {
+                    email: {
+                        required: "Пожалуйста, введите E-mail"
+                    },
+                    firstname: {
+                        required: "Пожалуйста, введите имя"
+                    },
+                    lastname: {
+                        required: "Пожалуйста, введите фамилию"
+                    },
+                    patronymic: {
+                        required: "Пожалуйста, введите отчество"
+                    },
+                    role: {
+                        required: "Пожалуйста, введите должность"
+                    },
+                    phone: {
+                        required: "Пожалуйста, введите телефон"
+                    },
+                }
             });
         });
     </script>
